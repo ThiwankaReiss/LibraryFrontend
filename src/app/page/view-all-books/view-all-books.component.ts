995c88f8a23b2e03173213a3c9ec70d61994e945
ThiwankaReiss <thiwankar2003@gmail.com>
2024-02-13 14:58:00 +0530

    fetching data from api and making student list

--- src/app/page/view-all-books/view-all-books.component.ts
+++ src/app/page/view-all-books/view-all-books.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-view-all-books',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule,FormsModule,CommonModule],
   templateUrl: './view-all-books.component.html',
   styleUrl: './view-all-books.component.css'
 })
-export class ViewAllBooksComponent {
+export class ViewAllBooksComponent implements OnInit{
+  private http;
+  public bookList:any;
 
+  constructor(private httpCient:HttpClient){
+    this.http=httpCient;
+  }
+  ngOnInit(): void {
+    this.loadBooks();
+  }
+
+  loadBooks(){
+    this.http.get("http://localhost:8080/book/get").subscribe((data)=>{
+      this.bookList=data;
+      console.log(this.bookList);
+    })
+  }
 }
